184238e092535266203ca6471cb88f5f0b79473b
Dmitriy <cvgfcvgfyt@gmail.com>
2023-12-20 22:07:03 +0400

    circles2

--- circles.py
+++ circles.py
@@ -1,15 +1,15 @@
 import random
 import sys
-from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtGui
+import ui
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, ui.Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         canvas = QtGui.QPixmap(600, 550)
         canvas.fill(Qt.white)
         self.label.setPixmap(canvas)
@@ -20,14 +20,14 @@ class MyWidget(QMainWindow):
         canvas = QtGui.QPixmap(600, 550)
         canvas.fill(Qt.white)
         self.label.setPixmap(canvas)
-
+        r, g, b = random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256)
         painter = QtGui.QPainter(self.label.pixmap())
         pen = QtGui.QPen()
         pen.setWidth(1)
-        pen.setColor(QtGui.QColor(255, 255, 0))
+        pen.setColor(QtGui.QColor(r, g, b))
         painter.setPen(pen)
         brush = QtGui.QBrush()
-        brush.setColor(QtGui.QColor(255, 255, 0))
+        brush.setColor(QtGui.QColor(r, g, b))
         brush.setStyle(Qt.BrushStyle.SolidPattern)
         painter.setBrush(brush)
         painter.begin(self)
